93bfd2d517d983a1564767cab68ebea4a060be8f
aaaa1597 <tokushi2011@gmail.com>
2025-08-16 21:20:57 +0900

    fragment追加

---
+++ app/src/main/java/com/aaa/recycleviewviewmodelsample/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.aaa.recycleviewviewmodelsample
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aaa.recycleviewviewmodelsample.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var _binding: FragmentMainBinding
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
