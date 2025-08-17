582f479d245f34313803fe22a581d51af2228601
aaaa1597 <tokushi2011@gmail.com>
2025-08-17 09:34:33 +0900

    RecycleView実装

--- app/src/main/java/com/aaa/recycleviewviewmodelsample/MainFragment.kt
+++ app/src/main/java/com/aaa/recycleviewviewmodelsample/MainFragment.kt
@@ -6,16 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.aaa.recycleviewviewmodelsample.databinding.FragmentMainBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
     private lateinit var _binding: FragmentMainBinding
+    private val _viewModel: MainViewModel by viewModels()
+    private var dataFlowJob: Job = Job()
 
-    companion object {
-        fun newInstance() = MainFragment()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /* StateFlow監視 */
+        dataFlowJob = lifecycleScope.launch { repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            _viewModel.dataFlow.collect { newData ->
+                (_binding.ryvSample.adapter as CustomAdapter).updateData(newData)
+            }
+        }}
     }
-
-    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
@@ -24,5 +38,51 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        /* RecyclerViewのレイアウト指定 */
+        _binding.ryvSample.layoutManager = LinearLayoutManager(requireContext())
+        /* アダプター定義 */
+        val adapter = CustomAdapter(mutableListOf())
+        _binding.ryvSample.adapter = adapter
+        _binding.btnAdd.setOnClickListener {
+            val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9') /* 使用する文字の種類 */
+            val addstr = (1..4)
+                .map { allowedChars.random() }
+                .joinToString("")
+            _viewModel.addItem(addstr)
+        }
+        _binding.btnDel.setOnClickListener {
+            _viewModel.removeItem(0)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        dataFlowJob.cancel()
+    }
+}
+
+class CustomAdapter(private var data: MutableList<String>) :
+        RecyclerView.Adapter<CustomAdapter.CustomViewHolder>() {
+    class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(android.R.id.text1)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(android.R.layout.simple_list_item_1, parent, false)
+        return CustomViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.textView.text = data[position]
+    }
+
+    override fun getItemCount() = data.size
+
+    /* データを更新するメソッド */
+    fun updateData(newData: List<String>) {
+        data.clear()
+        data.addAll(newData)
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/aaa/recycleviewviewmodelsample/MainViewModel.kt
+++ app/src/main/java/com/aaa/recycleviewviewmodelsample/MainViewModel.kt
@@ -1,7 +1,42 @@
 package com.aaa.recycleviewviewmodelsample
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _dataFlow = MutableStateFlow<List<String>>(mutableListOf())
+    val dataFlow = _dataFlow.asStateFlow()
+
+    init {
+        // 初期データを設定
+        val initialList = mutableListOf<String>()
+        for (idx in 1..3) {
+            initialList.add("アイテム$idx")
+        }
+        _dataFlow.value = initialList
+    }
+
+    /**
+     * リストに新しいアイテムを追加するメソッド
+     */
+    fun addItem(item: String) {
+        _dataFlow.update { currentList ->
+            currentList + item /* 新しいリストを生成して要素を追加 */
+        }
+    }
+
+    /**
+     * リストからアイテムを削除するメソッド
+     */
+    fun removeItem(position: Int) {
+        _dataFlow.update { currentList ->
+            if (position < currentList.size) {
+                currentList.toMutableList().apply { removeAt(position) }
+            } else {
+                currentList /* 変更がない場合は元のリストを返す */
+            }
+        }
+    }
 }
